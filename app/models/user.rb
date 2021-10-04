77d0ddc698fc9b9eb7be8e99b7fd0553e08455ad
irishelena <irissalvsantos@gmail.com>
2021-10-04 10:38:45 -0300

    Cotrollers e Validacao

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+    has_secure_password
+    before_save :email
+  
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
+    validates :nome, presence: true, length: { maximum: 50 }
+    validates :password, length: { minimum: 6 }
+    validates :email, presence: true, length: { maximum: 255 },
+                                      format: { with: VALID_EMAIL_REGEX },
+                                      uniqueness: { case_sensitive: true }
+
+end
