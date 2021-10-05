330dbeb09e6c80121b49d014e65c059845e9eb5d
irishsgs <iris.salvador@hominiss.com.br>
2021-10-05 11:09:10 -0300

    Final

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+
+    validates :nome, presence: true, lenght{ maximum: 50 }
+    validates :phone, presence: true
+
+    belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
                                       format: { with: VALID_EMAIL_REGEX },
                                       uniqueness: { case_sensitive: true }
 
+
+        has_many :contacts 
+
 end
